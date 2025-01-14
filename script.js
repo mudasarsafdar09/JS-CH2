a63dc2ed004ba547dd8f3d0b38e5905317ad5f33
md <mudasar077689@gmail.com>
2025-01-14 10:39:30 +0500

    js ch2 learn

---
+++ script.js
@@ -0,0 +1,26 @@
+// Artimetic Operators
+// let a = 5;
+// let b = 2;
+// // let c = a + b;
+// console.log("a = ", a, "& b = ", b, );
+// console.log("a + b = ", a + b);
+// console.log("a - b = ", a - b);
+// console.log("a * b = ", a * b);
+// console.log("a / b = ", a / b);
+// console.log("a % b = ", a % b);
+// console.log("a ** b = ", a ** b); //5^2 = 25
+
+// Unary Operator
+let a = 5;
+let b = 2;
+
+let c = 10;
+
+console.log("a = ", a, "& b = ", b, "& c = ", c,);
+a++;
+b--;
+console.log("a = ", a);
+console.log("b = ", b);
+
+console.log("c++ = ", c++); //10
+console.log("c = ", c); //v11
