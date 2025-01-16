50b3378601baa5679e6a7f7e90195cafa41f1a19
MDS-boy <mudasar077689@gmail.com>
2025-01-16 23:05:45 +0500

    Java Script leacture 2 complete with shrada khaphar Today

--- script.js
+++ script.js
@@ -149,4 +149,62 @@ if (num % 2 === 0) {
     console.log(num,"is odd");
 }
 
+let moode = "dark";
+let coloor;
+
+if(moode === "dark") {
+    coloor = "black";
+} else if(moode === "blue") {
+    coloor = "blue";
+} else if(moode === "pink")  {
+    coloor = "pink";
+} else {
+    coloor = "white";
+}
+
+console.log("coloor is",coloor);
+
+if(moode === "dark") {
+    console.log(moode);
+}
 //syntax -> rules
+
+{
+    let age = 25;
+
+    let result = age >= 18 ? "adult" : "not adult"; // Simler, compact if-else
+    console.log(result);
+}
+
+// Let's Practics Q1
+{
+let num = prompt("enter a number:");
+
+if(num % 5 === 0) {
+    console.log(num,"is a multiple of 5");
+} else {
+    console.log(num,"is NOT a multiple of 5");
+}
+// console.log(name);
+
+}
+
+//Q2 JS Program
+{
+let score = prompt("Enter your score (0-100)");
+let grade;
+
+if (score >= 90 && score <= 100) {
+    grade = "A";
+} else if(score >= 70 && score <= 89) {
+    grade = "B";
+} else if(score >=60 && ccore <= 69) {
+    grade = "C";
+} else if(score >= 50 && score <= 59) {
+    grade = "D";
+} else if(score >= 0 && score <= 49) {
+    grade = "F";
+}
+
+console.log("according to your scores, your grade was : ",grade);
+}
